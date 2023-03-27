d1025b78f5d51858eda0a61eee43e689c9f7ae93
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-27 19:00:20 -0300

    Validação do campo e-mail

--- projeto-lab-clothing-collection/src/app/pages/esqueci-senha/esqueci-senha.component.ts
+++ projeto-lab-clothing-collection/src/app/pages/esqueci-senha/esqueci-senha.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-esqueci-senha',
   templateUrl: './esqueci-senha.component.html',
-  styleUrls: ['./esqueci-senha.component.css']
+  styleUrls: ['./esqueci-senha.component.css'],
 })
-export class EsqueciSenhaComponent {
+export class EsqueciSenhaComponent implements OnInit {
+  emailForm!: FormGroup;
+  emailPattern: string =
+    '^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+).(.[a-z]{2,3})$';
+  constructor(private router: Router, private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.recuperarSenha();
+  }
+
+  recuperarSenha() {
+    this.emailForm = this.formBuilder.group({
+      loginEmail: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern),
+        ]),
+      ],
+    });
+  }
 }
