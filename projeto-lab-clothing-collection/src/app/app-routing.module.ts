b4bb67c0612ffa02f127ad5c5e58988308aacf35
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-27 17:30:05 -0300

    Páginas Content com o mesmo layout

--- projeto-lab-clothing-collection/src/app/app-routing.module.ts
+++ projeto-lab-clothing-collection/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
     path: '',
     component: ContentComponent,
     children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
       { path: 'login', component: LoginComponent },
       { path: 'esqueci-senha', component: EsqueciSenhaComponent },
     ],
