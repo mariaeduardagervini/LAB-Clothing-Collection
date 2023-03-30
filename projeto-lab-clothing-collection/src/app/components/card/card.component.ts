3a3890bf07c9e7eb053eba527c2332dd40c9b206
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-29 22:19:04 -0300

    Criação dos 3 cards principais do Dashboard

--- projeto-lab-clothing-collection/src/app/components/card/card.component.ts
+++ projeto-lab-clothing-collection/src/app/components/card/card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  @Input() public cards: any = {};
 }
--- projeto-lab-clothing-collection/src/app/pages/dashboard/dashboard.component.ts
+++ projeto-lab-clothing-collection/src/app/pages/dashboard/dashboard.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  cards = [
+    { titulo: 'Coleções', valor: 0 },
+    { titulo: 'Modelos', valor: 0 },
+    { titulo: 'Orçamento Médio (R$)', valor: 0 },
+  ];
 }
