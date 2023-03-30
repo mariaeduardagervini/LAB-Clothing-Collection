b88754e3b394e7c145e671eca3a3a502c33ff80d
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-30 13:44:41 -0300

    Construção tabela de Coleção + CSS + botao criar

--- projeto-lab-clothing-collection/src/app/pages/lista-colecoes/lista-colecoes.component.ts
+++ projeto-lab-clothing-collection/src/app/pages/lista-colecoes/lista-colecoes.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-lista-colecoes',
   templateUrl: './lista-colecoes.component.html',
-  styleUrls: ['./lista-colecoes.component.css']
+  styleUrls: ['./lista-colecoes.component.css'],
 })
 export class ListaColecoesComponent {
+  constructor(private router: Router) {}
 
+  criarColecao() {
+    this.router.navigate(['/criar-colecao']);
+  }
+  criar = {
+    titulo: 'Criar Coleção',
+    styles: {
+      width: '220px',
+      position: 'absolute',
+      top: '35px',
+      left: '75%',
+      margin: '-3% -4% -3% -3%',
+    },
+  };
 }
