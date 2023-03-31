5cf907db126c4853f8cda6a77e283f65247936f2
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-03-30 21:11:00 -0300

    11. Form criado e validado +  css +btn cancelar

--- projeto-lab-clothing-collection/src/app/pages/modelos/editar-modelo/editar-modelo.component.ts
+++ projeto-lab-clothing-collection/src/app/pages/modelos/editar-modelo/editar-modelo.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Modelo } from 'src/app/models/modelo';
 
 @Component({
   selector: 'app-editar-modelo',
   templateUrl: './editar-modelo.component.html',
-  styleUrls: ['./editar-modelo.component.css']
+  styleUrls: ['./editar-modelo.component.css'],
 })
-export class EditarModeloComponent {
+export class EditarModeloComponent implements OnInit {
+  changeModelo: Modelo = new Modelo();
+  editarModeloForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.editarModelo(new Modelo());
+  }
+  editarModelo(editarModelo: Modelo) {
+    this.editarModeloForm = this.formBuilder.group({
+      nome: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(2)]),
+      ],
+      tipo: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      colecao: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      responsavel: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(2)]),
+      ],
+      bordado: ['', Validators.required],
+      estampa: ['', Validators.required],
+    });
+  }
+  salvarModelo = {
+    titulo: 'Salvar',
+    styles: {
+      width: '180px',
+      position: 'absolute',
+      top: '',
+      left: '',
+      margin: '405px 5px 15px 950px',
+    },
+  };
+  cancelarModelo = {
+    titulo: 'Cancelar',
+    styles: {
+      color: '#A098AE',
+      backgroundColor: '#EBEBEB',
+      width: '180px',
+      position: 'absolute',
+      top: '',
+      left: '',
+      margin: '405px 5px 15px 720px',
+      border: 'none',
+    },
+  };
+  excluirModelo = {
+    titulo: 'Excluir',
+    styles: {
+      color: '#FFFFFF',
+      backgroundColor: '#FF4141',
+      width: '180px',
+      position: 'absolute',
+      top: '',
+      left: '',
+      margin: '405px 5px 15px 370px',
+      border: 'none',
+    },
+  };
+
+  onSubmit() {
+    this.router.navigate(['/lista-modelos']);
+  }
+  cancelar() {
+    this.router.navigate(['/lista-modelos']);
+  }
+  excluir() {
+    this.router.navigate(['/lista-modelos']);
+  }
 }
